8d8f798a1ab81bc4d9d2881f0737dec46ade9012
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-04-18 11:12:45 +0800

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by ChihYu Yeh on 2019/4/18.
+//  Copyright © 2019 cyyeh. All rights reserved.
+//
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
