67c7e759732644e58a1bd0471ef6f2e5fcc60ed1
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-04-18 20:42:21 +0800

    detail pop-up

---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,15 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by ChihYu Yeh on 2019/4/18.
+//  Copyright © 2019 cyyeh. All rights reserved.
+//
+
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+  override var shouldRemovePresentersView: Bool {
+    return false
+  }
+}
